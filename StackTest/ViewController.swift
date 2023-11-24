8fa6d781c4ddbe478382c3e1731d3031cf1d367b
Nick Kocharhook <nick@kocharhook.com>
2023-11-24 01:24:43 +0000

    Add content to the collection view. Add <= constraint to the collection view scroll view.

--- StackTest/ViewController.swift
+++ StackTest/ViewController.swift
@@ -8,13 +8,25 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    var itemCount = 15
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addItem(_ sender: Any) {
+        itemCount += 5
+        collectionView.reloadData()
+    }
+    
+    @IBAction func removeItems(_ sender: Any) {
+        itemCount -= 5
+        collectionView.reloadData()
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
@@ -24,3 +36,18 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSCollectionViewDataSource {
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemCount
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        guard let item = self.storyboard?.instantiateController(withIdentifier: "MyItem") as? NSCollectionViewItem else {
+            return NSCollectionViewItem()
+        }
+        
+        return item
+    }
+    
+    
+}
